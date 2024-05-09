5b2400599d4a33b6db6b8e2eb149774d6bc5fc9f
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2024-05-09 12:45:55 +0800

    Added main variable example

---
+++ errors_exceptions/main.py
@@ -0,0 +1,8 @@
+try:
+    f = open('testfile','w')
+    f.write('Test write this')
+except IOError:
+    # This will only check for an IOError exception and then execute this print statement
+    print("Error: Could not find file or read data")
+finally:
+   print('Always execute')
